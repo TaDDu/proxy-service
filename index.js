ad64308fb486ee261542946129246e0a21eef45c
Tatu Mehtäläinen <tatu.mehtalainen@gmail.com>
2018-12-13 14:52:56 +0200

    fix for env variables

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ var config = require("./config/config.json");
 var proxy = require("redbird")({ port: process.env.PORT || config.PORT });
 
 proxy.register(
-  process.env.DOMAIN || config.DOMAIN + "/api/login",
-  process.env.USERSERVICE || config.USERSERVICE + "/api/login"
+  process.env.DOMAIN + "/api/login" || config.DOMAIN + "/api/login",
+  process.env.USERSERVICE + "/api/login" || config.USERSERVICE + "/api/login"
 );
 proxy.register(
-  process.env.DOMAIN || config.DOMAIN + "/api/users",
-  process.env.USERSERVICE || config.USERSERVICE + "/api/users"
+  process.env.DOMAIN + "/api/users" || config.DOMAIN + "/api/users",
+  process.env.USERSERVICE + "/api/users" || config.USERSERVICE + "/api/users"
 );
 proxy.register(
   process.env.DOMAIN || config.DOMAIN,
   process.env.FRONTSERVICE || config.FRONTSERVICE + ":8080"
 );
 proxy.register(
-  process.env.DOMAIN || config.DOMAIN + "/a",
+  process.env.DOMAIN + "/a" || config.DOMAIN + "/a",
   process.env.FRONTSERVICE || config.FRONTSERVICE + ":8080"
 );
