b89d15f1a0c12704c17f122e244185966bf60397
Tatu Mehtäläinen <tatu.mehtalainen@gmail.com>
2018-12-13 10:34:24 +0200

    test

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require("dotenv").config();
 var config = require("./config/config.json");
 var proxy = require("redbird")({ port: process.env.PORT || config.PORT });
+/*
 config.routes.map(route => {
   proxy.register(
     process.env.DOMAIN || config.DOMAIN + route.from,
     process.env[route.DOMAIN] || config[route.DOMAIN] + route.to
   );
 });
+*/
+proxy.register(
+  process.env.DOMAIN + "/api/login",
+  process.env.USERSERVICE + "/api/login"
+);
+proxy.register(
+  process.env.DOMAIN + "/api/users",
+  process.env.USERSERVICE + "/api/users"
+);
