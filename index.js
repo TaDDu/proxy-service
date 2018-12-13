964625f22f540eefa31fb44805d73f3d140032c6
Tatu Mehtäläinen <tatu.mehtalainen@gmail.com>
2018-12-13 14:26:00 +0200

    Added frontend service

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 require("dotenv").config();
 var config = require("./config/config.json");
 var proxy = require("redbird")({ port: process.env.PORT || config.PORT });
-/*
-config.routes.map(route => {
-  proxy.register(
-    process.env.DOMAIN || config.DOMAIN + route.from,
-    process.env[route.DOMAIN] || config[route.DOMAIN] + route.to
-  );
-});
-*/
+
 proxy.register(
   process.env.DOMAIN || config.DOMAIN + "/api/login",
   process.env.USERSERVICE || config.USERSERVICE + "/api/login"
@@ -16,4 +9,12 @@ proxy.register(
 proxy.register(
   process.env.DOMAIN || config.DOMAIN + "/api/users",
   process.env.USERSERVICE || config.USERSERVICE + "/api/users"
+);
+proxy.register(
+  process.env.DOMAIN || config.DOMAIN,
+  process.env.FRONTSERVICE || config.FRONTSERVICE + ":8080"
+);
+proxy.register(
+  process.env.DOMAIN || config.DOMAIN + "/a",
+  process.env.FRONTSERVICE || config.FRONTSERVICE + ":8080"
 );
