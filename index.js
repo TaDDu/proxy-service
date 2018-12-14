df791389f7453772dc5fa9369ee08b4b4eecd796
Tatu Mehtäläinen <tatu.mehtalainen@gmail.com>
2018-12-14 13:49:51 +0200

    added weather service

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var proxy = require("redbird")({ port: process.env.PORT || config.PORT });
 var domain = process.env.DOMAIN || config.DOMAIN;
 var userservice = process.env.USERSERVICE || config.USERSERVICE;
 var iplocation = process.env.IPLOCATIONSERVICE || config.IPLOCATIONSERVICE;
+var weatherservice = process.env.WEATHERSERVICE || config.WEATHERSERVICE;
 var frontend = process.env.FRONTSERVICE || config.FRONTSERVICE;
 // LOGIN
 proxy.register(domain + "/api/login", userservice + "/api/login");
@@ -11,6 +12,9 @@ proxy.register(domain + "/api/users", userservice + "/api/users");
 
 // IP SERVICE
 proxy.register(domain + "/api/ip-location", iplocation + "/api/ip-location");
+
+// WEATHERSERVICE
+proxy.register(domain + "/api/weather", weatherservice + "/api/weather");
 
 // FRONTEND
 proxy.register(domain, frontend);
