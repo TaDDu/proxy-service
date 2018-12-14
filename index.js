f9be8e74f80d50922b5157f220f1148b95922801
Tatu Mehtäläinen <tatu.mehtalainen@gmail.com>
2018-12-14 11:23:06 +0200

    Added ip location service

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var config = require("./config/config.json");
 var proxy = require("redbird")({ port: process.env.PORT || config.PORT });
 
+// LOGIN
 proxy.register(
   process.env.DOMAIN + "/api/login" || config.DOMAIN + "/api/login",
   process.env.USERSERVICE + "/api/login" || config.USERSERVICE + "/api/login"
@@ -10,6 +11,15 @@ proxy.register(
   process.env.DOMAIN + "/api/users" || config.DOMAIN + "/api/users",
   process.env.USERSERVICE + "/api/users" || config.USERSERVICE + "/api/users"
 );
+
+// IP SERVICE
+proxy.register(
+  process.env.DOMAIN + "/api/ip-location" || config.DOMAIN + "/api/ip-location",
+  process.env.IPLOCATIONSERVICE + "/api/ip-location" ||
+    config.IPLOCATIONSERVICE + "/api/ip-locationn"
+);
+
+// FRONTEND
 proxy.register(
   process.env.DOMAIN || config.DOMAIN,
   process.env.FRONTSERVICE || config.FRONTSERVICE + ":8080"
