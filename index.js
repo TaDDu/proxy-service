c59fc15a83dc2d2390915c15415e80e6ae5bd1fc
Tatu Mehtäläinen <tatu.mehtalainen@gmail.com>
2018-12-18 10:15:19 +0200

    added geo service

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var userservice = process.env.USERSERVICE || config.USERSERVICE;
 var iplocation = process.env.IPLOCATIONSERVICE || config.IPLOCATIONSERVICE;
 var weatherservice = process.env.WEATHERSERVICE || config.WEATHERSERVICE;
 var taskservice = process.env.TASKSERVICE || config.TASKSERVICE;
+var geoservice = process.env.GEOSERVICE || config.GEOSERVICE;
 var frontend = process.env.FRONTSERVICE || config.FRONTSERVICE;
 // LOGIN
 proxy.register(domain + "/api/login", userservice + "/api/login");
@@ -19,5 +20,9 @@ proxy.register(domain + "/api/weather", weatherservice + "/api/weather");
 
 // TASKSERVICE
 proxy.register(domain + "/api/tasks", taskservice + "/api/tasks");
+
+//GEOSERVICE
+proxy.register(domain + "/api/geocoding", geoservice + "/api/geocoding");
+
 // FRONTEND
 proxy.register(domain, frontend);
