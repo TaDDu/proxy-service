1654c6df307a12ee03a641b4a367207ab161fccc
Tatu Mehtäläinen <tatu.mehtalainen@gmail.com>
2018-12-18 10:52:28 +0200

    enable ssl

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 require("dotenv").config();
 var config = require("./config/config.json");
-var proxy = require("redbird")({ port: process.env.PORT || config.PORT });
+//var proxy = require("redbird")({ port: process.env.PORT || config.PORT });
+
+var proxy = require("redbird")({
+  port: process.env.PORT || config.PORT, // http port is needed for LetsEncrypt challenge during request / renewal. Also enables automatic http->https redirection for registered https routes.
+  letsencrypt: {
+    path: __dirname + "/certs",
+    port: 9999 // LetsEncrypt minimal web server port for handling challenges. Routed 80->9999, no need to open 9999 in firewall. Default 3000 if not defined.
+  },
+  ssl: {
+    http2: true,
+    port: 443 // SSL port used to serve registered https routes with LetsEncrypt certificate.
+  }
+});
+
 var domain = process.env.DOMAIN || config.DOMAIN;
 var userservice = process.env.USERSERVICE || config.USERSERVICE;
 var iplocation = process.env.IPLOCATIONSERVICE || config.IPLOCATIONSERVICE;
@@ -25,4 +38,11 @@ proxy.register(domain + "/api/tasks", taskservice + "/api/tasks");
 proxy.register(domain + "/api/geocoding", geoservice + "/api/geocoding");
 
 // FRONTEND
-proxy.register(domain, frontend);
+proxy.register(domain, frontend, {
+  ssl: {
+    letsencrypt: {
+      email: process.env.EMAIL || "", // Domain owner/admin email
+      production: process.env.PRO || false // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
+    }
+  }
+});
